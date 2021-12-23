173b22ba7a439a3e295ed35d4c6d9346e34954b0
WPF498 <2766730850@qq.com>
2021-12-23 22:33:33 +0800

    my first commint

---
+++ test.c
@@ -0,0 +1,17 @@
+
+
+
+
+echo "# hello-world" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/WPF498/hello-world.git
+git push -u origin main
+
+
+
+git remote add origin https://github.com/WPF498/hello-world.git
+git branch -M main
+git push -u origin main
